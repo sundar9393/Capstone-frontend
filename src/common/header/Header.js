4f1e206fa80ff23668ac35147e411ebe734a4a66
sundar93 <sundar9393@gmail.com>
2020-03-14 01:08:08 +0530

    added login functionality

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -6,29 +6,60 @@ import Input from '@material-ui/core/Input';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import './Header.css';
 import Button from '@material-ui/core/Button';
-import { withStyles } from '@material-ui/core/styles';
- 
-const styles = theme => ({
-    input: {
-        '&::placeholder': {
-            color: 'white'
-          }
+import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Typography from '@material-ui/core/Typography';
+import Snackbar from '@material-ui/core/Snackbar';
+
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
     }
-})
+}
+
+const TabContainer = (props) => {
+    return (
+        <Typography component="div" style={{padding: 0, textAlign: "center"}}>
+            {props.children}
+        </Typography>
+    );
+}
+
+
+
 
 class Header extends Component {
 
     constructor() {
         super();
         this.state = {
-            moviesList: [{}]
+            moviesList: [{}],
+            modalIsOpen: false,
+            value: 0,
+            contactNum: "",
+            contactNumRequired: "dispNone",
+            isInvalidNumber: false,
+            password: "",
+            passwordRequired: "dispNone",
+            loggedIn: false,
+            openPopupMsg: false
         }
     }
 
     searchMoviesHandler = (e) => {
 
         if(e.target.value !== "") {
-            
+
         let that = this;
         let requestData = null;
         let searchMovies = new XMLHttpRequest();
@@ -47,6 +78,90 @@ class Header extends Component {
 
     } 
 
+    }
+
+    openModalHandler = () => {
+        this.setState({
+            modalIsOpen: true,
+            value: 0,
+            contactNum: "",
+            contactNumRequired: "dispNone",
+            password: "",
+            passwordRequired: "dispNone"
+        })
+    }
+
+    tabChangeHandler = (event, value) => {
+        this.setState({value})
+    }
+
+    inputContactNumChangeHandler = (e) => {
+        this.setState({contactNum: e.target.value})
+    }
+
+    inputPasswordChangeHandler = (e) => {
+        this.setState({password: e.target.value})
+    }
+
+    closeModalHandler = () => {
+        this.setState({modalIsOpen: false})
+    }
+
+    loginClickHandler = () => {
+
+        this.state.contactNum === "" ? this.setState({contactNumRequired: "dispBlock"}) :
+        this.setState({contactNumRequired: "dispNone"});
+        this.state.password === "" ? this.setState({passwordRequired: "dispBlock"}) :
+        this.setState({passwordRequired: "dispNone"});
+
+        if(this.state.password !== "" && this.state.password.length !== 10) {
+            this.setState({
+                isInvalidNumber: true,
+                contactNumRequired: "dispBlock"
+            })
+        }
+
+        if((this.state.contactNum === "") || (this.state.password === "") || (this.state.isInvalidNumber === true)) {
+            return;
+        }
+
+
+        //api call
+
+        let that=this;
+
+        let loginData =  null;
+        let loginAuthHeader = new Buffer(this.state.contactNum+':'+this.state.password).toString('base64');
+
+        let xhrlogin = new XMLHttpRequest();
+        xhrlogin.addEventListener("readystatechange", function() {
+
+            if(this.readyState === 4) {
+                
+                console.log(JSON.parse(this.responseText));                
+                sessionStorage.setItem("uuid", JSON.parse(this.responseText).id);
+                sessionStorage.setItem("access-token", xhrlogin.getResponseHeader("access-token"));                
+                that.setState({openPopupMsg: true})
+                that.closeModalHandler();
+
+            }
+        })
+
+        xhrlogin.open("POST", "http://localhost:8080/api/customer/login");
+        xhrlogin.setRequestHeader("authorization", "Basic "+loginAuthHeader);
+        xhrlogin.setRequestHeader("Cache-Control", "no-cache");
+        xhrlogin.send(loginData);
+
+        //Login successful message
+
+        
+
+
+        
+    }
+
+    snackBarCloseHandler = () => {
+       this.setState({openPopupMsg: false})     
     }
 
 
@@ -72,14 +187,80 @@ class Header extends Component {
                     color="default"
                     startIcon={<AccountCircleIcon />}
                     size="large"
+                    onClick={this.openModalHandler}
                     >
                         Login
                     </Button>
 
                 </header>
+
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login"
+                onRequestClose={this.closeModalHandler} style={customStyles}>
+
+                    <Tabs className="tabs" value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="Login" />
+                        <Tab label="Register" />
+                    </Tabs>
+
+                    {this.state.value === 0 &&
+                    <TabContainer>
+
+                        <FormControl required>
+
+                            <InputLabel htmlFor="contact">Contact No</InputLabel>
+                            <Input id="contact" type="text" contactNum={this.state.contactNum} 
+                                onChange={this.inputContactNumChangeHandler} />
+                            <FormHelperText className={this.state.contactNumRequired}>
+                                {this.state.isInvalidNumber === true ? <span className="red">Invalid Contact</span>
+                                : <span className="red">required</span> }                                     
+                            </FormHelperText>  
+
+                        </FormControl>
+                        <br /><br />
+                        <FormControl required>
+
+                            <InputLabel htmlFor="password">Password</InputLabel>
+                            <Input id="password" type="password" contactNum={this.state.password} 
+                                onChange={this.inputPasswordChangeHandler} />
+                            <FormHelperText className={this.state.passwordRequired}>
+                                    <span className="red">required</span>                               
+                            </FormHelperText>    
+
+                            <br /><br />
+                            <Button variant="contained" color="primary" onClick={this.loginClickHandler}>
+                                LOGIN
+                            </Button>
+
+                        </FormControl>
+
+                    </TabContainer>
+
+                    }
+                </Modal>
+
+
+                <Snackbar
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    autoHideDuration={5000}
+                    className="snackbar"
+                    open={this.state.openPopupMsg}
+                    onClose={this.snackBarCloseHandler}
+                    message={
+                        <span id="client-snackbar">
+                        <div className="confirm">
+                            <div className="message"> 
+                                Logged in successfully!!
+                            </div>
+                        </div>
+                        </span>
+                    }                    
+                />
             </div>
         )
     }
 }
 
-export default withStyles(styles)(Header);
+export default Header;
